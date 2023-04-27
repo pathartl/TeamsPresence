6db32e9902655d54aa7c0db98eb5b9b2a62edc7b
Nick Marnik <83248080+nick-marnik@users.noreply.github.com>
2023-04-27 10:34:38 -0400

    fix icons

--- HomeAssistantEntityStateUpdateAttributes.cs
+++ HomeAssistantEntityStateUpdateAttributes.cs
@@ -6,7 +6,7 @@ namespace TeamsPresence
     {
         [JsonProperty(PropertyName = "friendly_name")]
         public string FriendlyName { get; set; }
-        [JsonProperty(PropertyName = "state")]
+        [JsonProperty(PropertyName = "icon")]
         public string Icon { get; set; }
     }
 }
